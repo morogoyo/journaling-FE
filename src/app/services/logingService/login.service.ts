9a982ee60ed37c3e58c44c391a24849fd50a23af
Rene Torres <morogoyo@gmail.com>
2020-01-04 06:49:41 -0500

    added angular form controlls to the login html page, created service to login

--- src/app/services/logingService/login.service.ts
+++ src/app/services/logingService/login.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {UserInfo} from '../../model/user-info';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private userLoginURL = 'http://localhost:8080/authenticate'; // todo: check for backend url jwt
+  private userInfoObject: UserInfo;
 
-  constructor() { }
+  httpOptions = {
+    // tslint:disable-next-line:max-line-length
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Accept' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  userLogin(userLogin: UserInfo): Observable<UserInfo> {
+    console.log('userLogin method was triggered');
+    console.log(userLogin);
+    return this.http.post<UserInfo>(this.userLoginURL, userLogin, this.httpOptions);
+  }
 }
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {LoginService} from '../../services/logingService/login.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    constructor(private fb: FormBuilder, private httpService: LoginService) {
+    }
 
-  ngOnInit() {}
+    // todo this component will need a ssl certificate
+    loginForm = this.fb.group({
+
+        userName: [''],
+        password: ['']
+
+    });
+
+    onSubmit() {
+        // console.warn(this.registrationForm.value);
+        this.httpService.userLogin(this.loginForm.value).subscribe(d => console.log('logged-in User'));
+    }
+
+    ngOnInit() {
+    }
 
 }
